d323d778e6c24b8e5573e532d7e6a2e1998fd650
irgiadetia <irgiadetia8@gmail.com>
2026-03-16 22:36:04 +0800

    Delete service.py

--- service.py
+++
@@ -1,9 +0,0 @@
-import time
-import telebot
-TOKEN = "8688240904:AAFbm71rIxvaNTAuy0qUatSkAagp26uD6ZU"
-CHAT_ID = "5999516433"
-bot = telebot.TeleBot(TOKEN)
-
-while True:
-    # Service tetap hidup di background
-    time.sleep(60)
